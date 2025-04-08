6db331ecda62a5f1782a94a56f9d25e0a62ccfab
vinci <826296748@qq.com>
2025-04-08 21:22:32 +0800

    feat: 更新dify API域名和路径，添加聊天完成请求结构体

--- pkg/provider/chat/dify.go
+++ pkg/provider/chat/dify.go
@@ -1,10 +1,12 @@
 package chat
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 const (
-	difyDomain             = "api.dify.cn"
-	difyChatCompletionPath = "/v1/chat/chat-messages"
+	difyDomain             = "api.dify.ai"
+	difyChatCompletionPath = "/v1/completion-messages"
 )
 
 type difyProvider struct {
@@ -16,4 +18,17 @@ func (p *difyProvider) ShouldHandleRequest(ctx *gin.Context) bool {
 		return true
 	}
 	return false
+}
+
+// difyChatCompletionRequest represents the structure of a chat completion request.
+type difyChatCompletionRequest struct {
+	Model             string  `json:"model" validate:"required"`
+	Stream            bool    `json:"stream,omitempty"`
+	TokensToGenerate  int64   `json:"tokens_to_generate,omitempty"`
+	Temperature       float64 `json:"temperature,omitempty"`
+	TopP              float64 `json:"top_p,omitempty"`
+	MaskSensitiveInfo bool    `json:"mask_sensitive_info"`
+	// Messages          []minimaxMessage        `json:"messages" validate:"required,min=1"`
+	// BotSettings       []minimaxBotSetting     `json:"bot_setting" validate:"required,min=1"`
+	// ReplyConstraints  minimaxReplyConstraints `json:"reply_constraints" validate:"required"`
 }
